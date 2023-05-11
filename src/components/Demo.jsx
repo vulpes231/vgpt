6801bf49e2c35019971c1dc1339cb4b83f660c43
Vulpes231 <whykayshot@gmail.com>
2023-05-11 17:12:32 -0400

    initial commit of VGPT

---
+++ src/components/Demo.jsx
@@ -0,0 +1,62 @@
+import { useState, useEffect } from "react";
+import { BsArrowDownRight } from 'react-icons/bs'
+import {copy, loader, tick, linkIcon} from '../assets';
+import { useLazyGetSummaryQuery } from "../app/article";
+
+const Demo = () => {
+  const [article, setArticle] = useState({
+    url: '',
+    summary: '',
+  })
+
+  const [getSummary, {error, isFetching}] = useLazyGetSummaryQuery();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const { data } = await getSummary({
+      articleUrl: article.url,
+    })
+
+    if(data?.summary) {
+      const newArticle = {...article, summary:data.summary};
+      setArticle(newArticle);
+      console.log(newArticle);
+    }
+  }
+  return (
+    <section className="mt-16 w-full max-w-xl">
+      {/* search */}
+      <div className="flex flex-col w-full gap-2">
+        <form 
+          action="" 
+          className="relative flex justify-center items-center" onSubmit={handleSubmit}
+        >
+          <img 
+            src={linkIcon} 
+            alt="link-icon" 
+            className="absolute left-0 my-2 ml-3 w-5" 
+          />
+          <input 
+            type="text" 
+            placeholder="Enter a URL"
+            value={article.url}
+            onChange={(e) => setArticle({...article, url: e.target.value})}
+            required
+            className="url_input peer"
+          />
+          <button
+            type="button"
+            className="submit_btn peer-focus:border-gray-700 peer-focus:text-gray-700"
+          >
+            <BsArrowDownRight />
+          </button>
+        </form>
+        {/* Browse Url history */}
+      </div>
+      {/* display result */}
+      
+    </section>
+  )
+}
+
+export default Demo
